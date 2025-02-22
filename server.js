a00c63ebaf061474a366e4d54653efb718fabfa4
itay <itay2318@gmail.com>
2025-02-22 08:29:14 +0200

    init

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const tableName = 'games'
 const { createClient } = require("@supabase/supabase-js");
-const supabase = createClient(process.env.SUPABASE_KEY, process.env.SUPABASE_URL);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 app.use(cors());
 app.use(express.json());
 
 async function login(email, password) {
+
     const { user, session, error } = await supabase.auth.signInWithPassword({
         email,
         password,
     });
+
     if (error) {
-        console.error('Login error:', error.message);
+        // console.error('Login error:', error.message);
         return;
     }
+
     return session?.access_token;
 }
 
